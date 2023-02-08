9580ba54bd671b514a9310a998667b56617e3834
Edson Nogueira <edsoncezar16@gmail.com>
2023-02-08 13:26:35 -0300

    Fix duplicated columns resulting from join

--- scripts/query.py
+++ scripts/query.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
-import csv
+import pandas as pd
 from get_credentials import CREDENTIALS_PATH, get_db_credentials
 from sqlalchemy import create_engine, text, MetaData
 
 OUTPUT_DB_NAME = 'order_details'
+RESULTS_PATH = 'data/results.csv'
 
 _, user, password, port = get_db_credentials(CREDENTIALS_PATH)
 
@@ -12,15 +13,6 @@ engine = create_engine(
     f"postgresql://{user}:{password}@localhost/{OUTPUT_DB_NAME}",
     execution_options={'isolation_level': 'AUTOCOMMIT'}
 )
-metadata = MetaData()
-metadata.reflect(bind=engine)
-orders = metadata.tables['orders']
-order_details = metadata.tables['order_details']
-orders_columns = [column.name for column in orders.columns]
-order_details_columns = [column.name for column in order_details.columns]
-columns = orders_columns + order_details_columns
-
-conn = engine.connect()
 query = text(
     '''
     SELECT * 
@@ -29,11 +21,10 @@ query = text(
     ON ORDERS.ORDER_ID = ORDER_DETAILS.ORDER_ID
     '''
 )
-results = conn.execute(query)
-with open('data/results.csv', 'w', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerow(columns)
-    for row in results:
-        writer.writerow(row)
+conn = engine.connect()
+results = pd.read_sql_query(query, conn)
 conn.close()
 engine.dispose()
+results = results.T.drop_duplicates().T # to avoid repetition
+# resulting from the join operation
+results.to_csv(RESULTS_PATH, index=False)
