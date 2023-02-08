3c4380a09455f96f64916f9857279dc4f6670fa9
Edson Nogueira <edsoncezar16@gmail.com>
2023-02-08 12:51:56 -0300

    Got functional elements for a complete pipeline

--- scripts/transform.py
+++ scripts/transform.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
-import pandas as pd
+# treats the column 'shipped date' in orders table
+# which has 'nan' values that cannot be converted to a date type
 import sys
 import os
+import pandas as pd
 from datetime import datetime
 
 # Get the extraction date and check if the data is present
@@ -14,34 +16,28 @@ else:
     except:
         print('''
         Please provide a date in the format 'YYYY-MM-DD' or no date at 
-        all to extract today's data.
+        all to transform today's data.
         ''')
         sys.exit(1)
 extraction_date_str = extraction_date.strftime('%Y-%m-%d')
 date_folder_path = f'data/postgres/{extraction_date_str}'
-csv_folder_path = f'data/csv/{extraction_date_str}'
-loading_data_available = (os.path.exists(date_folder_path)) and \
-    (os.path.exists(csv_folder_path))
-if not loading_data_available:
+if not os.path.exists(date_folder_path):
     print('''
     There is no data in the local disk for the provided date.
     Plese consider extracting the respective date before attempting
-    to load it to the final database.
+    to transform it.
     ''')
     sys.exit(1)
 
 orders_data = pd.read_csv(
     f'{date_folder_path}/orders.csv'
 )
-order_details_data = pd.read_csv(
-    f'{csv_folder_path}/order_details.csv'
-)
-
-# treating the column 'shipped date' in orders table
-# which has 'nan' values that cannot be converted to a date type
-nan_dates = orders_data['shipped_date'].isna()
 orders_data['shipped_date'] = pd.to_datetime(
     orders_data['shipped_date'], errors='coerce'
 )
-print(orders_data.loc[nan_dates, ['order_id', 'shipped_date']])
-
+orders_data['shipped_date'].fillna(
+    pd.Timestamp('1970-01-01 00:00:00'), inplace=True
+)
+orders_data.to_csv(
+    f'{date_folder_path}/orders.csv', index=False
+)
