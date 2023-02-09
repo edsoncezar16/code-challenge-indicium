9525e141407d5337c4b3b6fa8b1ba26888446dd8
Edson Nogueira <edsoncezar16@gmail.com>
2023-02-09 15:15:20 -0300

    Fix small non-desired behaviors.

--- scripts/extract.py
+++ scripts/extract.py
@@ -2,7 +2,7 @@
 
 import pandas as pd
 from sqlalchemy import create_engine, inspect, MetaData
-import sys
+import warnings
 import os
 from utils import get_db_credentials, get_operation_date
 
@@ -18,22 +18,25 @@ if not os.path.exists(date_folder_path):
     os.makedirs(date_folder_path)
 if not os.path.exists(csv_folder_path):
     os.makedirs(csv_folder_path)
-    
+
 # extract data from the postgres database
 db_name, user, password, port = get_db_credentials(CREDENTIALS_PATH)
 engine = create_engine(f"postgresql://{user}:{password}@localhost:{port}/{db_name}")
 inspector = inspect(engine)
 table_names = inspector.get_table_names()
 total_tables = len(table_names)
-print("Extracting data from the database ...")
-for index, table_name in enumerate(table_names):
-    print(f"Getting data from table {table_name} ({index + 1}/{total_tables})")
-    table = pd.read_sql_table(table_name, engine.connect())
-    output_name = f"{date_folder_path}/{table_name}.csv"
-    print(f"Writting data from table {table_name} to local disk.")
-    table.to_csv(output_name, index=False)
-engine.dispose()  # to prevent resource leaks
-print("Done.\n")
+with warnings.catch_warnings(): # temporary fix for annoying messages
+    # regarding bpchar data type not being recognized by sqlalchemy
+    warnings.simplefilter('ignore')
+    print("Extracting data from the database ...")
+    for index, table_name in enumerate(table_names):
+        print(f"Getting data from table {table_name} ({index + 1}/{total_tables})")
+        table = pd.read_sql_table(table_name, engine.connect())
+        output_name = f"{date_folder_path}/{table_name}.csv"
+        print(f"Writting data from table {table_name} to local disk.")
+        table.to_csv(output_name, index=False)
+    engine.dispose()  # to prevent resource leaks
+    print("Done.\n")
 
 # extract data from the provided csv file
 print("Extracting data from the csv file ...")
