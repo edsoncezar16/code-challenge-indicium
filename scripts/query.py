1eb49eeb6e04109e69094df5b35e7d2bc874c409
Edson Nogueira <edsoncezar16@gmail.com>
2023-02-08 21:26:56 -0300

    Improve code formatting.

--- scripts/query.py
+++ scripts/query.py
@@ -6,37 +6,37 @@ import sys
 from utils import get_db_credentials
 from sqlalchemy import create_engine, text, MetaData
 
-CREDENTIALS_PATH = os.environ['CREDENTIALS_PATH']
-OUTPUT_DB_NAME = os.environ['OUTPUT_DB_NAME']
-RESULTS_PATH = os.environ['RESULTS_PATH']
+CREDENTIALS_PATH = os.environ["CREDENTIALS_PATH"]
+OUTPUT_DB_NAME = os.environ["OUTPUT_DB_NAME"]
+RESULTS_PATH = os.environ["RESULTS_PATH"]
 
 _, user, password, port = get_db_credentials(CREDENTIALS_PATH)
 
 engine = create_engine(
     f"postgresql://{user}:{password}@localhost/{OUTPUT_DB_NAME}",
-    execution_options={'isolation_level': 'AUTOCOMMIT'}
+    execution_options={"isolation_level": "AUTOCOMMIT"},
 )
 query = text(
-    '''
+    """
     SELECT * 
     FROM ORDERS
     JOIN ORDER_DETAILS
     ON ORDERS.ORDER_ID = ORDER_DETAILS.ORDER_ID
-    '''
+    """
 )
 try:
     conn = engine.connect()
     results = pd.read_sql_query(query, conn)
     conn.close()
     engine.dispose()
-    results = results.T.drop_duplicates().T # to avoid repetition
+    results = results.T.drop_duplicates().T  # to avoid repetition
     # resulting from the join operation
     results.to_csv(RESULTS_PATH, index=False)
-except: # output database is not set
+except:  # output database is not set
     print(
-        '''
+        """
         Output database is not set. Please consider loading data to output
         database before querying.
-        '''
+        """
     )
     sys.exit(1)
