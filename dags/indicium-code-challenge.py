3099c4377e8076dc4d440eb677f71f18603c601d
Edson Nogueira <edsoncezar16@gmail.com>
2023-03-24 14:59:29 -0300

    Got working extract from northwind db task.

--- dags/indicium-code-challenge.py
+++ dags/indicium-code-challenge.py
@@ -1,22 +1,25 @@
-from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
+
 import pandas as pd
+from sqlalchemy import inspect
+
+from datetime import datetime, timedelta
 import os
 from collections import namedtuple
 
 NamedTable = namedtuple("NamedTable", "name data")
 
 INPUT_CONN_ID = 'northwind-db'
-LFS_PATH = '/indicium-lfs'
+LFS_PATH = '/opt/airflow/indicium-lfs'
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'start_date': datetime(2023, 3, 22),
     'retries': 1,
-    'retry_delay': timedelta(minutes=5)
+    'retry_delay': timedelta(minutes=1)
 }
 
 dag = DAG(
@@ -27,29 +30,27 @@ dag = DAG(
 
 def extract_tables():
     tables = []
-    conn = PostgresHook(conn_name_attr=INPUT_CONN_ID).get_conn()
-    cur = conn.cursor()
-    get_table_names_query = '''
-    SELECT table_name 
-    FROM information_schema.tables
-    WHERE table_schema='public'
-    '''
-    cur.execute(get_table_names_query)
-    table_names = [row[0] for row in cur.fetchall()]
-    cur.close()
+    db_hook = PostgresHook(postgres_conn_id=INPUT_CONN_ID)
+    engine = db_hook.get_sqlalchemy_engine()
+    inspector = inspect(engine)
+    table_names = inspector.get_table_names()
     for table_name in table_names:
-        table = NamedTable(table_name, pd.read_sql_table(table_name, conn))
+        table = NamedTable(
+            name=table_name,
+            data=pd.read_sql_table(table_name, engine.connect())
+        )
         tables.append(table)
     return tables
 
-def tables_to_files(tables):
-    logical_date = (datetime.today() - timedelta(days=1)).date()
-    folder_path = f"{LFS_PATH}/postgres/{logical_date}"
+def tables_to_files(**context):
+    tables = context['task_instance'].xcom_pull(task_ids='extract')
+    logical_date = datetime.today() - timedelta(days=1)
+    folder_path = f"{LFS_PATH}/postgres/{logical_date.strftime('%Y-%m-%d')}"
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
     for table in tables:
         csv_path = f"{folder_path}/{table.name}.csv"
-        table.data.to_csv(csv_path, index=False)
+        pd.DataFrame(table.data).to_csv(csv_path, index=False)
 
 extract = PythonOperator(
     task_id='extract',
@@ -60,7 +61,7 @@ extract = PythonOperator(
 load = PythonOperator(
     task_id='load',
     python_callable=tables_to_files,
-    opt_kw={'tables': extract.output},
+    op_kwargs={'tables': extract.output},
     dag=dag
 )
 
