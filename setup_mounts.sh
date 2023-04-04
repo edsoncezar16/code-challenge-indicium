bc93c3407f03512d3991cc165ec9071440b18023
Edson Nogueira <edsoncezar16@gmail.com>
2023-04-04 15:48:20 -0300

    Got ACI group up and running, fix setup mounts.

--- setup_mounts.sh
+++ setup_mounts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cp -r dags/ /mnt/dockeracivolumes/dags/
 cp -r . /mnt/dockeracivolumes/sources/
-cp analytics-config/postgresql.conf /mnt/dockeracivolumes/analytics-config/
+cp analytics_config/postgresql.conf /mnt/dockeracivolumes/analytics-config/
 cp data/northwind.sql /mnt/dockeracivolumes/init-db-data/
